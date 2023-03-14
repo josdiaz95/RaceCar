7aa77418e41761d89a561efa31eb270f9d4fa8e7
josdiaz95 <josdiaz95@outlook.com>
2023-03-13 18:55:33 -0600

    Add files via upload

---
+++ RaceCarMidTerm/RaceTrack/Cars/ChevyImpala.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RaceTrack.RaceTrack.Cars;
+
+public class ChevyImpala : RaceCar
+{
+    public ChevyImpala()
+    {
+        Name = "Chevy Impala";
+        TopSpeed = 160;
+    }
+
+    public override void StartEngine()
+    {
+        Console.WriteLine($"The {Name} sounds like a monster!"); ;
+    }
+
+    public override void Brake()
+    {
+        Console.WriteLine($"The {Name} goes so fast that it screeches when it brakes.");
+        base.Brake();
+    }
+}
---
+++ RaceCarMidTerm/RaceTrack/Drivers/Dean.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using RaceTrack.RaceTrack.Cars;
+
+namespace RaceTrack.RaceTrack.Drivers;
+
+public class Dean : Driver
+{
+    public Dean(RaceCar car) : base(car)
+    {
+        Name = "Dean Winchester";
+        SkillLevel = 10;
+    }
+
+    public override void Drive()
+    {
+        Car.Accelerate(SkillLevel);
+    }
+}
